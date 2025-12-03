0fad3585757bff636ded6f02a8588be96b3a7a82
Saúl <saul.prigarci@gmail.com>
2025-12-03 13:23:48 +0100

    Añadidos dos métodos nuevos, uno nos imprime el estado y otro no lo muestra, añadidos tambien algunos comentarios al código

--- Ordenador.java
+++ Ordenador.java
@@ -1,9 +1,16 @@
-
+/**
+ * Primera clase creada desde 0 en Programación de 1ºDAM
+ * La clase simula un ordenador, teniendo dueño, bateria y si esta o no encendido.
+ * 
+ * @author Saúl Prieto García (@saulprigarci)
+ * @version 1.0 (03/12/25)
+ */
 public class Ordenador {
 
-    private String nombreDueno;
-    private int porcentajeBateria;
-    private boolean encendido;
+    // Atributos de la clase Ordenador.
+    private String nombreDueno; // Atributo de tipo cadena String.
+    private int porcentajeBateria; // Atributo de tipo int.
+    private boolean encendido; // Atributo de tipo boolean.
 
     public Ordenador(String elNombre, int laBateria) {
         nombreDueno = elNombre;
@@ -37,5 +44,15 @@ public class Ordenador {
         }else{
             encendido = true;
         }
+    }
+
+    public void imprimirEstado() {
+        System.out.println("Dueño: " + nombreDueno);
+        System.out.println("Porcentaje de Bateria: " + porcentajeBateria + "%");
+        System.out.println("Encendido: " + encendido);
+    }
+
+    public String getEstado() {
+        return "Dueño: " + nombreDueno + " | Porcentaje de Bateria: " + porcentajeBateria + "% | Encendido: " + encendido;
     }
 }
